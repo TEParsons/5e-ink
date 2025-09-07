98b2e013763bf1a7b7d0cabec11be2890f2d83ca
Todd Parsons <todd.e.parsons@googlemail.com>
2025-09-07 15:23:41 +0100

    enh: Use svg for proficiency so it's font agnostic

--- src/lib/ui/ctrls/Proficiency.svelte
+++ src/lib/ui/ctrls/Proficiency.svelte
@@ -8,10 +8,10 @@
     } = $props()
 
     let options = {
-        0: {icon: "⚪︎", label: "⚪︎ Not proficient"},
-        0.5: {icon: "◐︎", label: "◐︎ Semi-proficient"},
-        1: {icon: "⏺︎", label: "⏺︎ Proficient"},
-        2: {icon: "⚉︎", label: "⚉︎ Expert proficient"},
+        0: {icon: "none", label: "Not proficient"},
+        0.5: {icon: "half", label: "Semi-proficient"},
+        1: {icon: "full", label: "Proficient"},
+        2: {icon: "expert", label: "Expert proficient"},
     }
 
     let order = [
@@ -33,8 +33,11 @@
 <button
     class=proficiency-ctrl
     onclick={evt => dialog.shown = true}
+    aria-label={options[value || 0].icon}
 >
-    {options[value || 0].icon}
+    <svg class=icon>
+        <use xlink:href="assets/proficiency/{options[value || 0].icon}.svg" />
+    </svg>
 </button>
 
 <Dialog
@@ -53,6 +56,9 @@
             class:selected={value === option}
             onclick={evt => value = option}
         >
+            <svg class=icon>
+                <use xlink:href="assets/proficiency/{options[option].icon}.svg" />
+            </svg>
             {options[option].label}
         </button>
     {/each}
@@ -68,8 +74,16 @@
         line-height: 1em;
     }
 
+    svg.icon {
+        height: 1rem;
+        width: 1rem;
+    }
 
     .option {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: .5rem;
         background-color: var(--base);
         color: var(--text-on-base);
         border: 1px solid var(--crust);
