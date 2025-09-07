d19abec0cd792155a45a54db3bd533bde4d29cae
Todd Parsons <todd.e.parsons@googlemail.com>
2025-09-07 11:03:14 +0100

    sys: Setup static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +6,23 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: './build',
+			assets: './build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? "" : "/5e-ink",
+			assets: process.argv.includes('dev') ? "" : "https://teparsons.github.io/5e-ink",
+			relative: true
+		},
+		prerender: {
+			entries: ['*'],  
+		},
 	}
 };
 
+export const prerender = true;
 export default config;
