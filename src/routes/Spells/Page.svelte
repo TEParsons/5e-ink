3b751b3b558310a57c251ce8397eef74e06361b2
TEParsons <todd.e.parsons@gmail.com>
2025-10-09 21:23:33 +0100

    fix: Spells from non-class sources were missed

--- src/routes/Spells/Page.svelte
+++ src/routes/Spells/Page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { getContext } from "svelte";
     import { SlotsCtrl } from "$lib/ui/ctrls";
-    import { getTotalSlots } from "$lib/utils";
+    import { getTotalSlots, getAdvancements, sentenceCase } from "$lib/utils";
     import Spell from "./Spell.svelte"
     import SpellStatsCtrl from "./SpellStatsCtrl.svelte";
 
@@ -16,13 +16,11 @@
         <h2>Cantrips</h2>
     </div>
     <div class=spell-list>
-        {#each Object.keys(stats.class) as cls}
-            {#each Object.entries(stats.class[cls].levels) as [lvl, advancements]}
-                {#each Object.entries(advancements.casting?.cantrips || []) as [i, cantrip]}
-                    <Spell 
-                        bind:spell={stats.class[cls].levels[lvl].casting.cantrips[i]}
-                    />
-                {/each}
+        {#each getAdvancements(stats) as advancement}
+            {#each Object.keys(advancement.casting?.cantrips || []) as i}
+                <Spell 
+                    bind:spell={advancement.casting.cantrips[i]}
+                />
             {/each}
         {/each}
     </div>
@@ -30,7 +28,7 @@
     {#each Object.keys(stats.current.slots) as level}
         <div class=level-header>
             <h2>
-                {`${level[0].toUpperCase()}${level.slice(1)} level`}
+                {`${sentenceCase(level)} level`}
             </h2>
             <SlotsCtrl 
                 bind:value={stats.current.slots[level]}
@@ -38,36 +36,14 @@
             />
         </div>
         <div class=spell-list>
-            {#each Object.keys(stats.class) as cls}
-                <!-- from levelling -->
-                {#each Object.entries(stats.class[cls].levels) as [lvl, advancement]}
-                    {#each Object.entries(advancement.casting?.spells || []) as [i, spell]}
-                        {#if spell.level === level}
-                            <Spell 
-                                bind:spell={stats.class[cls].levels[lvl].casting.spells[i]}
-                            />
-                        {/if}
-                    {/each}
+            {#each getAdvancements(stats) as advancement}
+                {#each Object.entries(advancement.casting?.spells || []) as [i, spell]}
+                    {#if spell.level === level}
+                        <Spell 
+                            bind:spell={advancement.casting.spells[i]}
+                        />
+                    {/if}
                 {/each}
-                <!-- from subtype (bard college, paladin oath, etc.) -->
-                {#if stats.class[cls].subtype?.advancements && stats.class[cls].subtype?.advancements.some(
-                    val => val.casting?.spells?.some(
-                        spell => spell.level === level
-                    )
-                )}
-                    <div class=subclass-header>
-                        {stats.class[cls].subtype.name}:
-                    </div>
-                    {#each Object.entries(stats.class[cls].subtype.advancements || []) as [adv, advancement]}
-                        {#each Object.entries(advancement.casting?.spells || []) as [i, spell]}
-                            {#if spell.level === level}
-                                <Spell 
-                                    bind:spell={stats.class[cls].subtype.advancements[adv].casting.spells[i]}
-                                />
-                            {/if}
-                        {/each}
-                    {/each}
-                {/if}
             {/each}
         </div>
     {/each}
